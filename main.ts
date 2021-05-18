18b8768678b92d55069ceeba8e471b91eef35013
045665 <84044136+045665@users.noreply.github.com>
2021-05-18 14:08:39 +1000

    Update main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+namespace SpriteKind {
+    export const end = SpriteKind.create()
+    export const fortress = SpriteKind.create()
+}
 function gravity () {
     if (mario.isHittingTile(CollisionDirection.Bottom)) {
         if (velY > 0) {
@@ -8,11 +12,6 @@ function gravity () {
     }
     mario.setVelocity(0, velY)
 }
-// animState vars:
-// 0 = idle
-// 1 = walk
-// 2 = jump
-// 
 // dir vars:
 // 0 = left
 // 1 = right
@@ -104,8 +103,8 @@ function flip () {
     for (let value34 of chungusWalkAnim) {
         value34.flipX()
     }
-    for (let value34 of upgradeAnim1) {
-        value34.flipX()
+    for (let value342 of upgradeAnim1) {
+        value342.flipX()
     }
 }
 function breakBlock () {
@@ -151,6 +150,37 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         music.playMelody("G C5 - - - - - - ", 1000)
     }
 })
+function victorySong () {
+    music.playTone(196, 133)
+    music.playTone(262, 133)
+    music.playTone(330, 133)
+    music.playTone(392, 133)
+    music.playTone(523, 133)
+    music.playTone(659, 133)
+    music.playTone(784, music.beat(BeatFraction.Whole))
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.playTone(208, 133)
+    music.playTone(262, 133)
+    music.playTone(311, 133)
+    music.playTone(415, 133)
+    music.playTone(523, 133)
+    music.playTone(622, 133)
+    music.playTone(831, music.beat(BeatFraction.Whole))
+    music.playTone(659, music.beat(BeatFraction.Whole))
+    music.playTone(233, 133)
+    music.playTone(294, 133)
+    music.playTone(349, 133)
+    music.playTone(466, 133)
+    music.playTone(587, 133)
+    music.playTone(698, 133)
+    music.playTone(932, music.beat(BeatFraction.Whole))
+    music.playTone(932, 133)
+    music.playTone(932, 133)
+    music.playTone(932, 133)
+    music.playTone(1046.50, music.beat(BeatFraction.Double))
+    music.rest(music.beat(BeatFraction.Double))
+    game.over(true)
+}
 function shroomBounceLoop () {
     if (activeShroom) {
         if (activeShroom.isHittingTile(CollisionDirection.Left)) {
@@ -266,42 +296,10 @@ function initSongs () {
     0,
     659,
     0,
-    1046.50,
+    1046.5,
     0,
-    1046.50,
-    1046.50,
-    0,
-    0,
-    0,
-    0,
-    0,
-    784,
-    740,
-    698,
-    622,
-    0,
-    659,
-    0,
-    415,
-    440,
-    523,
-    0,
-    440,
-    523,
-    587,
-    0,
-    0,
-    622,
-    0,
-    0,
-    587,
-    0,
-    0,
-    523,
-    0,
-    0,
-    0,
-    0,
+    1046.5,
+    1046.5,
     0,
     0,
     0,
@@ -323,6 +321,22 @@ function initSongs () {
     587,
     0,
     0,
+    622,
+    0,
+    0,
+    587,
+    0,
+    0,
+    523,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
     784,
     740,
     698,
@@ -330,10 +344,26 @@ function initSongs () {
     0,
     659,
     0,
-    1046.50,
+    415,
+    440,
+    523,
     0,
-    1046.50,
-    1046.50,
+    440,
+    523,
+    587,
+    0,
+    0,
+    784,
+    740,
+    698,
+    622,
+    0,
+    659,
+    0,
+    1046.5,
+    0,
+    1046.5,
+    1046.5,
     0,
     0,
     0,
@@ -607,6 +637,13 @@ function loadAnims () {
     fireFlowerAnim = assets.animation`fireFlowerAnim`
     upgradeAnim1 = assets.animation`upgradeAnim1`
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.end, function (sprite, otherSprite) {
+    victory = 1
+    music.setTempo(150)
+    music.setVolume(200)
+    mario.x = 3168
+    victorySong()
+})
 function walkSpeed () {
     if (controller.B.isPressed()) {
         controller.moveSprite(mario, 120, 0)
@@ -616,14 +653,13 @@ function walkSpeed () {
 }
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mario.tileKindAt(TileDirection.Bottom, assets.tile`VPTL1`) || mario.tileKindAt(TileDirection.Bottom, assets.tile`VPTR1`)) {
-        game.reset()
+        downPipe()
     }
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     upgrading = 1
     otherSprite.destroy()
     if (otherSprite == activeShroom) {
-        mario.y += -16
         MarioState = 1
         animState = -1
     } else {
@@ -632,6 +668,19 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSpr
     PowerUpSound()
     upgrading = 0
 })
+function downPipe () {
+    goingDownPipe = 1
+    animation.runImageAnimation(
+    mario,
+    assets.animation`goDownPipe`,
+    100,
+    false
+    )
+    music.playMelody("G - G - G - - - ", 300)
+    scene.setBackgroundColor(15)
+    tiles.placeOnRandomTile(mario, sprites.castle.tilePath3)
+    goingDownPipe = 0
+}
 function shroomGravity (shroom: Sprite) {
     shroom.setVelocity(80, 0)
     shroom.ay = 1000
@@ -640,6 +689,9 @@ function shroomGravity (shroom: Sprite) {
 }
 let noteNum2 = 0
 let noteNum = 0
+let startedVictoryAnim = 0
+let isBig = 0
+let goingDownPipe = 0
 let upgrading = 0
 let fireFlowerAnim: Image[] = []
 let overworldBase: number[] = []
@@ -658,11 +710,16 @@ let chungusWalkAnim: Image[] = []
 let walkAnim: Image[] = []
 let animState = 0
 let velY = 0
+let victory = 0
 let MarioState = 0
 let dir = 0
 let mario: Sprite = null
 scene.setBackgroundColor(9)
 tiles.setTilemap(tilemap`level1`)
+let fortress2 = sprites.create(assets.image`Fortress`, SpriteKind.fortress)
+tiles.placeOnRandomTile(fortress2, sprites.builtin.forestTiles0)
+let flag = sprites.create(assets.image`Flag`, SpriteKind.end)
+flag.setPosition(3172, 140)
 mario = sprites.create(assets.image`startMario`, SpriteKind.Player)
 tiles.placeOnTile(mario, tiles.getTileLocation(5, 13))
 loadAnims()
@@ -671,66 +728,107 @@ scene.cameraFollowSprite(mario)
 dir = 1
 let Music = 1
 MarioState = 0
-let isBig = 0
+victory = 0
 music.setTempo(210)
 game.onUpdate(function () {
-    if (!(upgrading)) {
-        animate()
-        gravity()
-        walkSpeed()
-        breakBlock()
-        shroomBounceLoop()
+    if (!(victory)) {
+        if (!(upgrading) && !(goingDownPipe)) {
+            animate()
+            gravity()
+            walkSpeed()
+            breakBlock()
+            shroomBounceLoop()
+        } else if (!(goingDownPipe)) {
+            mario.setVelocity(0, 0)
+            controller.moveSprite(mario, 0, 0)
+            if (!(isBig)) {
+                mario.y += -16
+                animation.runImageAnimation(
+                mario,
+                assets.animation`upgradeAnim1`,
+                200,
+                true
+                )
+                isBig = 1
+            }
+        }
     } else {
         mario.setVelocity(0, 0)
         controller.moveSprite(mario, 0, 0)
-        if (!(isBig)) {
-            animation.runImageAnimation(
-            mario,
-            assets.animation`upgradeAnim1`,
-            200,
-            true
-            )
-            isBig = 1
+        if (mario.y < 216 && MarioState == 0 || mario.y < 208 && MarioState == 1) {
+            mario.y += 2
+            if (MarioState == 0) {
+                mario.setImage(assets.image`marioSlide`)
+            } else {
+                mario.setImage(assets.image`chungusMarioSlide`)
+            }
+        } else {
+            if (!(startedVictoryAnim)) {
+                startedVictoryAnim = 1
+                if (MarioState == 0) {
+                    animation.runImageAnimation(
+                    mario,
+                    assets.animation`marioWalk`,
+                    100,
+                    true
+                    )
+                } else {
+                    animation.runImageAnimation(
+                    mario,
+                    assets.animation`chungusMarioWalkAnim`,
+                    100,
+                    true
+                    )
+                }
+            }
+            mario.setVelocity(50, 0)
+            if (mario.x > 3264) {
+                mario.destroy()
+            }
         }
     }
 })
 forever(function () {
-    if (!(upgrading)) {
-        noteNum = overworldMain[noteNumOWM]
-        if (noteNum > 4 || noteNum == 0) {
-            music.playTone(noteNum, music.beat(BeatFraction.Half))
-        } else if (noteNum > 1) {
-            if (noteNum == 2) {
-                music.playTone(392, 185)
-            } else if (noteNum == 3) {
-                music.playTone(659, 185)
-            } else {
-                music.playTone(784, 185)
+    if (!(victory)) {
+        if (!(upgrading) && !(goingDownPipe)) {
+            noteNum = overworldMain[noteNumOWM]
+            if (noteNum > 4 || noteNum == 0) {
+                music.playTone(noteNum, music.beat(BeatFraction.Half))
+            } else if (noteNum > 1) {
+                if (noteNum == 2) {
+                    music.playTone(392, 185)
+                } else if (noteNum == 3) {
+                    music.playTone(659, 185)
+                } else {
+                    music.playTone(784, 185)
+                }
             }
-        }
-        noteNumOWM += 1
-        if (noteNumOWM > overworldMain.length) {
-            noteNumOWM = 0
+            noteNumOWM += 1
+            if (noteNumOWM > overworldMain.length) {
+                noteNumOWM = 0
+            }
         }
     }
 })
 forever(function () {
-    if (!(upgrading)) {
-        noteNum2 = overworldBase[noteNumOWB]
-        if (noteNum2 > 4 || noteNum2 == 0) {
-            music.playTone(noteNum2, music.beat(BeatFraction.Half))
-        } else if (noteNum2 > 1) {
-            if (noteNum2 == 2) {
-                music.playTone(165, 185)
-            } else if (noteNum2 == 3) {
-                music.playTone(262, 185)
-            } else {
-                music.playTone(330, 185)
+    if (!(victory)) {
+        if (!(upgrading) && !(goingDownPipe)) {
+            noteNum2 = overworldBase[noteNumOWB]
+            if (noteNum2 > 4 || noteNum2 == 0) {
+                music.playTone(noteNum2, music.beat(BeatFraction.Half))
+            } else if (noteNum2 > 1) {
+                if (noteNum2 == 2) {
+                    music.playTone(165, 185)
+                } else if (noteNum2 == 3) {
+                    music.playTone(262, 185)
+                } else {
+                    music.playTone(330, 185)
+                }
             }
-        }
-        noteNumOWB += 1
-        if (noteNumOWB > overworldBase.length) {
-            noteNumOWB = 0
+            noteNumOWB += 1
+            if (noteNumOWB > overworldBase.length) {
+                noteNumOWB = 0
+            }
         }
     }
 })
